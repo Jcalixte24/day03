9dfce1fc434d64ec9b2b89150e26bf9383d03299
Jcalixte24 <123331116+Jcalixte24@users.noreply.github.com>
2025-01-08 15:15:00 +0100

    exo17.R

--- exo17.R
+++ exo17.R
@@ -1,3 +1,8 @@
-notes= data.frame(Nom = c('Alice', 'Bob', 'Clara'), Note1 = c(15, 10, 12) , Note2 = c(17, 14, 9))
-notes$Moyenne <- rowMeans(notes[, c("Note1", "Note2")]) #rowMeans nous permet de nous focus sur les lignes qui sont les valeurs
+
+notes <- data.frame(Nom = c('Alice', 'Bob', 'Clara'),
+                    Note1 = c(15, 10, 12),
+                    Note2 = c(17, 14, 9))
+
+notes$Moyenne <- apply(notes[, c("Note1", "Note2")], 1, mean)
+
 print(notes)
