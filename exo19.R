058f0f397e33a4f72189f850d28f03a69f8af348
Jcalixte24 <123331116+Jcalixte24@users.noreply.github.com>
2025-01-08 15:15:32 +0100

    exo19.R

--- exo19.R
+++ exo19.R
@@ -1,5 +1,4 @@
 library(ggplot2)
 ventes = data.frame(Mois =c( "Janvier","Fevrier","Mars","Avril") ,ChiffreAffaires = c(1000, 1200, 1500, 1700))
 
-ggplot(ventes, aes(x= Mois, y= ChiffreAffaires , group = 1)) +
-  geom_line()+geom_point() + ggtitle("Graphe chiffre d'affaires par mois")
+ggplot(ventes, aes(x= Mois, y= ChiffreAffaires , group = 1)) +geom_line() + ggtitle("Graphe chiffre d'affaires par mois")
